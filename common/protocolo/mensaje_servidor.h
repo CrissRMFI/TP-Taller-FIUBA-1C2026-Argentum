0cba3c84a56965db97c79a039ede7426dcf0e9c0
CrissERMFI <croldan@fi.uba.ar>
2026-05-09 00:12:46 -0300

    feat( mensajes servidor ): Agregamos un struct con el mensaje del estado completo del personaje.

---
+++ common/protocolo/mensaje_servidor.h
@@ -0,0 +1,21 @@
+#ifndef MENSAJE_SERVIDOR_H
+#define MENSAJE_SERVIDOR_H
+
+#include <cstdint>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "./opcode.h"
+
+struct MensajeEstadoPersonaje {
+    uint16_t vidaActual;
+    uint16_t vidaMax;
+    uint16_t manaActual;
+    uint16_t manaMax;
+    uint32_t oro;
+    uint8_t  nivel;
+    uint32_t experiencia;
+};
+
+#endif
