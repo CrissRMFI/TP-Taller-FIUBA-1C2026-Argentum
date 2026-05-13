20255aa9131f22f321ae6f064d4f5b6d4735efa0
CrissERMFI <croldan@fi.uba.ar>
2026-05-13 00:05:59 -0300

    Dejo el esqueleto del juego, falta implementar todas las funciones privadas del juego, solo están las firmas.

--- server/game/juego.h
+++ server/game/juego.h
@@ -8,8 +8,32 @@
 #include "../gameloop/mensaje_salida.h"
 
 class Juego {
-public:
+  public:
     std::list<MensajeSalida> ejecutarComando(const uint16_t idCliente, const ComandoJugador& comando);
+  
+ private:
+    std::list<MensajeSalida> ejecutarMeditar(uint16_t idCliente);
+    std::list<MensajeSalida> ejecutarResucitar(uint16_t idCliente);
+    std::list<MensajeSalida> ejecutarTomar(uint16_t idCliente);
+    std::list<MensajeSalida> ejecutarRevisarClan(uint16_t idCliente);
+    std::list<MensajeSalida> ejecutarDejarClan(uint16_t idCliente);
+    std::list<MensajeSalida> ejecutarMover(uint16_t idCliente, const ComandoMover& comando);
+    std::list<MensajeSalida> ejecutarAtacar(uint16_t idCliente, const ComandoAtacar& comando);
+    std::list<MensajeSalida> ejecutarTirar(uint16_t idCliente, const ComandoTirar& comando);
+    std::list<MensajeSalida> ejecutarEquipar(uint16_t idCliente, const ComandoEquipar& comando);
+    std::list<MensajeSalida> ejecutarComprar(uint16_t idCliente, const ComandoComprar& comando);
+    std::list<MensajeSalida> ejecutarVender(uint16_t idCliente, const ComandoVender& comando);
+    std::list<MensajeSalida> ejecutarDepositarItem(uint16_t idCliente,const ComandoDepositarItem& comando);
+    std::list<MensajeSalida> ejecutarDepositarOro(uint16_t idCliente,const ComandoDepositarOro& comando);
+    std::list<MensajeSalida> ejecutarRetirarItem(uint16_t idCliente,const ComandoRetirarItem& comando);
+    std::list<MensajeSalida> ejecutarRetirarOro(uint16_t idCliente,const ComandoRetirarOro& comando);
+    std::list<MensajeSalida> ejecutarListar(uint16_t idCliente, const ComandoListar& comando);
+    std::list<MensajeSalida> ejecutarCurar(uint16_t idCliente, const ComandoCurar& comando);
+    std::list<MensajeSalida> ejecutarChatGlobal(uint16_t idCliente,const ComandoChatGlobal& comando);
+    std::list<MensajeSalida> ejecutarChatPrivado(uint16_t idCliente,const ComandoChatPrivado& comando);
+    std::list<MensajeSalida> ejecutarFundarClan(uint16_t idCliente,const ComandoFundarClan& comando);
+    std::list<MensajeSalida> ejecutarUnirseClan(uint16_t idCliente,const ComandoUnirseClan& comando);
+    std::list<MensajeSalida> ejecutarGestionMiembroClan(uint16_t idCliente,const ComandoGestionMiembreClan& comando);
 };
 
 #endif
