53aa46e30ce7dce66802a42c519cacfe9d26cf08
CrissERMFI <croldan@fi.uba.ar>
2026-05-17 18:40:38 -0300

    fix( jugador.h): Elimino la clase POsicion y raza, porque las definimos en otro archivo, para mejorar la lectura.

--- server/game/jugador.h
+++ server/game/jugador.h
@@ -1,14 +1,15 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
-#include <string>
 #include <cstdint>
+#include <string>
+#include <vector>
 
-#include "inventario.h"
-#include "../gameloop/mensaje_salida.h"
-#include "item.h"
+#include "objeto/inventario.h"
+#include "modelo/posicion.h"
+#include "modelo/raza.h"
+#include "modelo/clase_personaje.h"
 
-// Pongo aca para que sea mas facil de manejar, luego hay que moverlos
 enum class Estado {
     Vivo,
     Fantasma,
@@ -16,49 +17,30 @@ enum class Estado {
     Resucitando
 };
 
-enum class Clase {
-    Guerrero,
-    Mago,
-    Clerigo,
-    Paladin
-};
-
-enum class Raza {
-    Humano,
-    Elfo,
-    Enano,
-    Gnomo
-};
-
-struct Posicion {
-    uint16_t x;
-    uint16_t y;
-    uint16_t mapaId;
-};
-
 class Jugador {
 public:
-    Jugador(const std::string& nombre, Clase clase, Raza raza, Posicion posicion);
-    // Modificadores de Vida y Mana
-    void recibir_danio(uint16_t cantidad); // Anula meditacion
+    Jugador(const std::string& nombre, ClasePersonaje clase, Raza raza, Posicion posicion);
+
+    // Modificadores de vida y maná
+    void recibir_danio(uint16_t cantidad);
     void curar(uint16_t cantidad);
     void recuperar_mana(uint16_t cantidad);
 
-    // Experiencia y Progresión
+    // Experiencia y progresión
     void ganar_experiencia(uint32_t cantidad);
     void sumar_oro(uint32_t cantidad);
     bool gastar_oro(uint32_t cantidad);
 
-    // Movimiento y Estado
-    void mover_a(uint16_t x, uint16_t y); // Anula meditacion
+    // Movimiento y estado
+    void mover_a(uint16_t x, uint16_t y);
     void resucitar(uint16_t x, uint16_t y);
     void meditar();
     uint16_t calcular_danio();
 
-    //Objetos e Inventario
-    bool agregar_item(const uint16_t idItem);
-    bool eliminar_item(const uint16_t idItem);
-    bool equipar_item(const uint16_t idItem);
+    // Inventario
+    bool agregar_item(uint16_t idItem);
+    bool eliminar_item(uint16_t idItem);
+    bool equipar_item(uint16_t idItem);
     void agregar_item_banco(uint16_t idItem);
     void agregar_oro_banco(uint32_t cantidad);
     bool sacar_item_banco(uint16_t idItem);
@@ -66,19 +48,19 @@ public:
 
     // Getters
     uint16_t getId() const;
-    bool es_newbie() const; // Para el fair play
-    uint8_t getNivel() const; // Para el fair play
+    bool es_newbie() const;
+    uint8_t getNivel() const;
     bool fundo_clan() const;
     std::string getNombre() const;
     Posicion getPosicion() const;
     Estado getEstado() const;
-    std::vector<Item> getItemsBanco() const;
+    std::vector<uint16_t> getIdItemsBanco() const;
     uint32_t getOroBanco() const;
     uint16_t getClan() const;
 
 private:
     uint16_t idJugador;
-    uint16_t idClan; // 0 si no tiene clan
+    uint16_t idClan;
     std::string nombre;
     uint8_t nivel;
     uint32_t experiencia;
@@ -97,18 +79,17 @@ private:
     uint8_t constitucion;
     Posicion posicion;
     Inventario inventario;
-    std::vector<Item> itemsBanco;
-    Clase clase;
+    std::vector<uint16_t> idItemsBanco;
+    ClasePersonaje clase;
     Estado estado;
     Raza raza;
-    
-    bool fundo_clan;
+    bool fundadoClan;
 
     void subirNivel();
-    void morir(); // Deberia devolver lo que dropea
+    void morir();
     void perder_experiencia(uint32_t cantidad);
     bool consumir_mana(uint16_t cantidad);
-    void consumir_item(const uint16_t idItem); // Cuando se equipa un item consumible
+    void consumir_item(uint16_t idItem);
     bool esquiva_ataque();
     bool es_golpe_critico();
 };
