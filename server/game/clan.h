11d24404a0af07a6431717836ec8e939c123c00b
CrissERMFI <croldan@fi.uba.ar>
2026-05-17 19:36:01 -0300

    feat( clan.h): Agrego metodos bool para saber si un jugador es fundador, esta baneado o en solicitud pendiente. Tambien para conocer la cantidad de miembros, nombre y estados.

--- server/game/clan.h
+++ server/game/clan.h
@@ -18,14 +18,22 @@ struct MiembroClan {
 
 class Clan {
 public:
-    Clan(const std::string& nombre, uint16_t idFundador);
+    Clan(uint16_t idClan, const std::string& nombre, uint16_t idFundador);
 
     void agregarMiembro(const uint16_t& idJugador);
     void pedirUnirse(const uint16_t& idJugador);
     void eliminarMiembro(const uint16_t& idJugador);
     void banearMiembro(const uint16_t& idJugador);
-    bool esMiembro(const uint16_t& idJugador) const;
-    std::vector<uint16_t> obtenerMiembros() const;
+
+    bool esMiembro(const uint16_t& idJugador)    const;
+    bool esFundador(const uint16_t& idJugador)   const;
+    bool estaBaneado(const uint16_t& idJugador)  const;
+    bool estaPendiente(const uint16_t& idJugador) const;
+
+    size_t cantidadMiembros()                    const;
+    std::string getNombre()                      const;
+    std::vector<uint16_t> obtenerMiembros()      const;
+    std::vector<uint16_t> obtenerPendientes()    const;
 
 private:
     uint16_t idClan;
