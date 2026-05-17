6e8674bcc444cf9b480c124d6f45310f69b98d32
CrissERMFI <croldan@fi.uba.ar>
2026-05-17 18:42:30 -0300

    fix(criatura.h): Eliminamos la clase posicion.

--- server/game/criatura.h
+++ server/game/criatura.h
@@ -1,11 +1,11 @@
 #ifndef CRIATURA_H
 #define CRIATURA_H
 
-#include <string>
 #include <cstdint>
-#include <vector>
+#include <map>
 
-// Pongo aca para que sea mas facil de manejar, luego hay que moverlos
+#include "modelo/posicion.h"
+
 enum class TipoCriatura {
     Goblin,
     Esqueleto,
@@ -15,21 +15,16 @@ enum class TipoCriatura {
     Golem
 };
 
-struct Posicion {
-    uint16_t x;
-    uint16_t y;
-    uint16_t mapaId;
-};
-
 class Criatura {
 public:
-    friend class Juego; // Para que el juego pueda modificar si esta persiguiendo a un jugador
+    friend class Juego;
+
     uint16_t getId() const;
     Posicion getPos() const;
     uint8_t getAggro() const;
     uint8_t calcularDanio() const;
-    std::vector<bool, uint16_t> estaPersiguiendo();
-    void mover(Posicion& nuevaPosicion) const; // El juego mueve a la criatura dependiendo si esta cerca del jugador o no
+    std::map<uint16_t, bool> estaPersiguiendo() const;
+    void mover(Posicion nuevaPosicion);
 
 private:
     uint16_t idCriatura;
@@ -43,8 +38,7 @@ private:
     uint8_t rangoAggro;
     uint8_t danioMin;
     uint8_t danioMax;
-    std::vector<bool, uint16_t> persiguiendoJugador;
-    
+    std::map<uint16_t, bool> persiguiendoJugador;
 };
 
-#endif // CRIATURA_H
+#endif
