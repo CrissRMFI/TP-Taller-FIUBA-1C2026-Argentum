6f759a5a61899e723ceb857b8f46ccb4d227ef1b
Rama-Besse <rbesse@fi.uba.ar>
2026-05-23 12:53:47 +0000

    Hot Fix a conexion servidor

--- client/red/conexion_servidor.cpp
+++ client/red/conexion_servidor.cpp
@@ -4,13 +4,10 @@
 
 ConexionServidor::ConexionServidor(const char* hostname, const char* servname)
     : protocolo(Socket(hostname, servname)),
-      colaEntrante(),
-      colaSaliente(),
       receptor(protocolo, &colaEntrante),
       emisor(protocolo, &colaSaliente) {}
 
 void ConexionServidor::iniciar() {
-    std::cout << "[conexion] establecida\n";
     receptor.start();
     emisor.start();
 }
@@ -20,7 +17,6 @@ void ConexionServidor::detener() {
     emisor.stop();
     protocolo.cerrarConexion();
     colaSaliente.close();
-    colaEntrante.close();
 }
 
 void ConexionServidor::esperar() {
