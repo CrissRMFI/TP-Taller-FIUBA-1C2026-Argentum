30a6ee3378ca548d0af5ee80978969c73b0dd68a
CrissERMFI <croldan@fi.uba.ar>
2026-06-06 21:10:55 -0300

    feat(chat): COnfiguraicon cliente cabvecera

--- client/config/config_cliente.h
+++ client/config/config_cliente.h
@@ -2,6 +2,8 @@
 #define CLIENT_CONFIG_CONFIG_CLIENTE_H
 
 #include <cstdint>
+#include <string>
+#include <vector>
 
 struct ConfigCliente {
     bool     vsync = true;
@@ -11,6 +13,19 @@ struct ConfigCliente {
     int  ancho = 640;
     int  alto = 480;
     bool fullscreen = false;
+
+    std::string fuenteRuta = "fonts/font.ttf";
+    std::string fondoChatRuta = "chat/fondoPrincipal.png";
+    int fuenteTam = 14;
+    int chatMaxLineas = 6;
+    int chatPanelX = 8;
+    int chatPanelY = 8;
+    int chatPanelAncho = 380;
+    int chatPanelAlto = 120;
+    std::vector<std::string> ayudaChat;
+    std::vector<int> chatColorTexto = {255, 255, 255};
+    std::vector<int> chatColorInput = {255, 255, 0};
+    std::vector<int> chatColorAyuda = {200, 200, 200};
 };
 
 #endif
