4d4dadb03b4eb60f34464334ab0ab587b2a1a61a
CrissERMFI <croldan@fi.uba.ar>
2026-06-07 04:11:45 -0300

    Agregamos la capacidas de comprar y vender par ael comerciante

--- common/game/npc/comerciante.cpp
+++ common/game/npc/comerciante.cpp
@@ -11,20 +11,22 @@ std::map<uint16_t, std::pair<uint8_t, uint8_t>> Comerciante::listarItemsDisponib
     return itemsDisponibles;
 }
 
+// El comerciante le COMPRA al jugador (el jugador vende): el jugador recibe el precio de venta (el menor). itemsDisponibles guarda {precioCompraJugador, precioVentaJugador}.
 std::pair<bool, uint8_t> Comerciante::comprarItem(uint16_t idItem) {
     if (itemsDisponibles.find(idItem) == itemsDisponibles.end()) {
         return {false, 0};
     }
 
-    uint8_t precioCompra = itemsDisponibles[idItem].first;
-    return {true, precioCompra};
+    uint8_t precioVentaJugador = itemsDisponibles[idItem].second;
+    return {true, precioVentaJugador};
 }
 
+// El comerciante le VENDE al jugador (el jugador compra): el jugador paga el precio de compra (el mayor).
 std::pair<bool, uint8_t> Comerciante::venderItem(uint16_t idItem) {
     if (itemsDisponibles.find(idItem) == itemsDisponibles.end()) {
         return {false, 0};
     }
 
-    uint8_t precioVenta = itemsDisponibles[idItem].second;
-    return {true, precioVenta};
+    uint8_t precioCompraJugador = itemsDisponibles[idItem].first;
+    return {true, precioCompraJugador};
 }
