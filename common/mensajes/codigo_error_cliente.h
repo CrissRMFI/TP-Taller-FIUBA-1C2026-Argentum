b2de9e197f568bbb88b73f03c6526f9c36e7ebc9
CrissERMFI <croldan@fi.uba.ar>
2026-06-06 23:09:11 -0300

    feat(objetos9: Agrego codsigos de error del cliente

--- common/mensajes/codigo_error_cliente.h
+++ common/mensajes/codigo_error_cliente.h
@@ -7,6 +7,9 @@ enum class CodigoErrorCliente : uint8_t {
     CONFIG_NO_LEIDA = 0,
     FUENTE_NO_CARGADA = 1,
     FONDO_CHAT_NO_CARGADO = 2,
+    ITEMS_NO_LEIDOS = 3,
+    ICONO_ITEM_NO_CARGADO = 4,
+    PANEL_NO_CARGADO = 5,
 };
 
 #endif
