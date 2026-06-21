184c3bd47f68e42405b251b2a976c130089477a7
CrissERMFI <croldan@fi.uba.ar>
2026-06-20 22:34:44 -0300

    agrego el struct de tipo d emenaaje

---
+++ common/protocolo/categoria_chat.h
@@ -0,0 +1,12 @@
+#ifndef CATEGORIA_CHAT_H
+#define CATEGORIA_CHAT_H
+
+#include <cstdint>
+
+enum class CategoriaChat : uint8_t {
+    Normal  = 0, // chat global
+    Privado = 1, // chat privado (@)
+    Sistema = 2, // aviso del juego sin emisor
+};
+
+#endif
